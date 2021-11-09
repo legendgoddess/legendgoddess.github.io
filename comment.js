e164b01ae5ab38207adc5f19f7f812be48a99f5f
Rkkr <i@ruakker.cn>
2021-11-09 13:41:06 +0800

    Update comment.js

--- comment.js
+++ comment.js
@@ -9,7 +9,7 @@ const md5 = require("md5");
 // 根据自己的情况进行配置
 const config = {
     username: "legendgoddess", // GitHub 用户名
-    token: "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",  // GitHub Token
+    token: "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",  // GitHub Token
     repo: "blog",  // 存放 issues 的 git 仓库
     // sitemap.xml的路径，commit.js放置在根目录下，无需修改，其他情况自行处理
     sitemapUrl: path.resolve(__dirname, "./public/sitemap.xml"),
